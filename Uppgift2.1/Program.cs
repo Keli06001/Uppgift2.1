1e9a34c27b1e0cc62518da35bde2803ef8207a0e
Keli06001 <keli06001@utb.vaxjo.se>
2023-09-15 12:34:29 +0200

    Add project files.

---
+++ Uppgift2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uppgift2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din favoritbok?");
+            string bok = Console.ReadLine();
+            Console.WriteLine(bok);
+        }
+    }
+}
